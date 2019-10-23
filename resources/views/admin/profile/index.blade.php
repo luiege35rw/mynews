b9fa37f9f0ac502d1c1b914c3d4461275cf1e795
”渡部亮介” <luiege35rw@yahoo.co.jp>
2019-10-23 09:09:58 +0000

    ヘロク導入

--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -32,8 +32,10 @@
                         <thead>
                             <tr>
                                 <th width="10%">ID</th>
-                                <th width="20%">タイトル</th>
-                                <th width="50%">本文</th>
+                                <th width="20%">名前</th>
+                                <th width="20%">性別</th>
+                                <th width="20%">趣味</th>
+                                <th width="50%">自己紹介</th>
                                 <th width="10%">操作</th>
                             </tr>
                         </thead>
@@ -41,8 +43,10 @@
                             @foreach($posts as $profile)
                                 <tr>
                                     <th>{{ $profile->id }}</th>
-                                    <td>{{ \Str::limit($profile->title, 100) }}</td>
-                                    <td>{{ \Str::limit($profile->body, 250) }}</td>
+                                    <td>{{ \Str::limit($profile->name, 100) }}</td>
+                                    <td>{{ \Str::limit($profile->gender, 100) }}</td>
+                                    <td>{{ \Str::limit($profile->hobby, 100) }}</td>
+                                    <td>{{ \Str::limit($profile->introduction, 250) }}</td>
                                     <td>  
                                        <div>
                                             <a href="{{ action('Admin\ProfileController@edit', ['id' => $profile->id]) }}">編集</a>
